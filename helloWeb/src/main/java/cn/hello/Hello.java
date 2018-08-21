0fc5a20600fa756b709263785551f95eeb84ffd1
zhangleo <zhangchaojun@knet.cn>
2018-08-21 14:17:38 +0800

    初始化

---
+++ helloWeb/src/main/java/cn/hello/Hello.java
@@ -0,0 +1,14 @@
+package cn.hello;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
